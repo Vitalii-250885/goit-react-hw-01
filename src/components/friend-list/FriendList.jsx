47d94613a49c33a323839d266a5af5703190cd50
Vitalii Protsko <provitaleks@gmail.com>
2024-03-18 10:26:39 +0200

    Update FriendList.jsx

--- src/components/friend-list/FriendList.jsx
+++ src/components/friend-list/FriendList.jsx
@@ -4,13 +4,13 @@ import css from "./FriendList.module.css";
 const FriendList = (friends) => {
   return (
     <ul className={css.friendList}>
-      {Object.values(friends)[0].map((friend) => {
+      {Object.values(friends)[0].map(({ avatar, name, isOnline, id }) => {
         return (
           <FriendListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-            key={friend.id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
           />
         );
       })}
